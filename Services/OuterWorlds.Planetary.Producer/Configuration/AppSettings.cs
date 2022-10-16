74f7c6a0af94d5c79971a72d731d185693effd21
Driuthorn <douglas.amaral@hotmail.com>
2022-10-16 14:39:27 -0300

    Implements dynamic usage of appsettings

---
+++ Services/OuterWorlds.Planetary.Producer/Configuration/AppSettings.cs
@@ -0,0 +1,39 @@
+﻿using Configuration.AppSettingsModels;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Configuration
+{
+    public class AppSettings
+    {
+        public static AppSettings Settings => ReadSettings<AppSettings>($"{AppDomain.CurrentDomain.BaseDirectory}appsettings.json");
+
+
+        [JsonProperty("applicationName")]
+        public string ApplicationName { get; set; }
+
+        [JsonProperty("rabbitMQSettings")]
+        public RabbitMQSettings RabbitMQSettings { get; set; }
+
+        #region Reader
+
+        private static T ReadSettings<T>(string filename)
+        {
+            T settingsObject;
+
+            if (!File.Exists(filename))
+                throw new Exception($"{filename} not found");
+
+            using(var reader = new StreamReader(filename))
+            {
+                var json = reader.ReadToEnd();
+                settingsObject = JsonConvert.DeserializeObject<T>(json);
+            }
+
+            return settingsObject;
+        }
+
+        #endregion
+    }
+}
---
+++ Services/OuterWorlds.Planetary.Producer/Configuration/AppSettingsModels/RabbitMQSettings.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+
+namespace Configuration.AppSettingsModels
+{
+    public class RabbitMQSettings
+    {
+        [JsonProperty("host")]
+        public string Host { get; set; }
+
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("port")]
+        public int Port { get; set; }
+
+        [JsonProperty("virtualHost")]
+        public string VirtualHost { get; set; }
+
+        [JsonProperty("requestHeartBeatInSeconds")]
+        public ushort RequestHeartBeatInSeconds { get; set; }
+
+        [JsonProperty("exchange")]
+        public string Exchange { get; set; }
+
+        [JsonProperty("queue")]
+        public string Queue { get; set; }
+
+        [JsonProperty("prefetchCount")]
+        public ushort PrefetchCount { get; set; }
+
+        [JsonProperty("prefetch")]
+        public ushort Prefetch { get; set; }
+
+        [JsonProperty("global")]
+        public bool Global { get; set; }
+
+        [JsonProperty("autoAck")]
+        public bool AutoAck { get; set; }
+
+        [JsonProperty("connectionName")]
+        public string ConnectionName { get; set; }
+    }
+}
